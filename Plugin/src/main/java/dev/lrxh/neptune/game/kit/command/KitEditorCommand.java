8a2c076c0f82bf5942ec35911afd59b17ec3c530
mallusrgreat <mallusrgreat@duck.com>
2025-10-06 20:48:05 +0530

    /kiteditor edit and jdk25 support (#116)

--- Plugin/src/main/java/dev/lrxh/neptune/game/kit/command/KitEditorCommand.java
+++ Plugin/src/main/java/dev/lrxh/neptune/game/kit/command/KitEditorCommand.java
@@ -6,10 +6,12 @@ import dev.lrxh.neptune.API;
 import dev.lrxh.neptune.configs.impl.MessagesLocale;
 import dev.lrxh.neptune.game.kit.Kit;
 import dev.lrxh.neptune.game.kit.menu.editor.KitEditorMenu;
+import dev.lrxh.neptune.game.kit.menu.editor.button.KitEditorSelectButton;
 import dev.lrxh.neptune.profile.data.ProfileState;
 import dev.lrxh.neptune.profile.impl.Profile;
 import dev.lrxh.neptune.providers.clickable.Replacement;
 import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.ClickType;
 
 import java.util.Arrays;
 
@@ -22,6 +24,15 @@ public class KitEditorCommand {
             return;
         if (profile.hasState(ProfileState.IN_LOBBY, ProfileState.IN_PARTY)) {
             new KitEditorMenu().open(player);
+        }
+    }
+
+    @Command(name = "edit", desc = "", usage = "<kit>")
+    public void edit(@Sender Player player, Kit kit) {
+        if (player == null) return;
+        Profile profile = API.getProfile(player);
+        if (profile.hasState(ProfileState.IN_LOBBY, ProfileState.IN_PARTY)) {
+            new KitEditorSelectButton(0, kit).onClick(ClickType.LEFT, player);
         }
     }
 
