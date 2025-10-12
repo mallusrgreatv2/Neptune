902460d9395377e6591bb29e58f87034e57e572b
Devlrxxh <seabearvalorant@gmail.com>
2025-10-12 09:13:55 +0200

    Fixed multiple matches ongoing in 1 match

--- Plugin/src/main/java/dev/lrxh/neptune/game/kit/Kit.java
+++ Plugin/src/main/java/dev/lrxh/neptune/game/kit/Kit.java
@@ -195,7 +195,7 @@ public class Kit implements IKit {
         for (Arena arena : arenas) {
             if (!arena.isEnabled())
                 continue;
-            if (arena.isUsed() && SettingsLocale.ARENA_DUPLICATES.getBoolean())
+            if (arena.isUsed() && !SettingsLocale.ARENA_DUPLICATES.getBoolean())
                 continue;
             if (!arena.isSetup() || !arena.isDoneLoading())
                 continue;
