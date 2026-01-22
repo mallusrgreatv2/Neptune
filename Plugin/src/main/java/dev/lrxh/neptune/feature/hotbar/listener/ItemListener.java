4cb48e366a6155afa7152b5212f24f1dcc468588
mallusrgreat <mallusrgreat@duck.com>
2026-01-22 18:21:52 +0530

    Fix NPE when no inventory in hotbar listener (#146)

--- Plugin/src/main/java/dev/lrxh/neptune/feature/hotbar/listener/ItemListener.java
+++ Plugin/src/main/java/dev/lrxh/neptune/feature/hotbar/listener/ItemListener.java
@@ -19,7 +19,6 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerSwapHandItemsEvent;
 import org.bukkit.inventory.ItemStack;
 
-
 public class ItemListener implements Listener {
 
     @EventHandler
@@ -31,12 +30,16 @@ public class ItemListener implements Listener {
                 && profile.getState() != ProfileState.IN_SPECTATOR) {
             return;
         }
-        if (player.getGameMode().equals(GameMode.CREATIVE)) return;
+        if (player.getGameMode().equals(GameMode.CREATIVE))
+            return;
 
-        if (profile.getState() == ProfileState.IN_CUSTOM) return;
+        if (profile.getState() == ProfileState.IN_CUSTOM)
+            return;
         event.setCancelled(true);
-        if (event.getItem() == null) return;
-        if (event.getItem().getType().equals(Material.AIR)) return;
+        if (event.getItem() == null)
+            return;
+        if (event.getItem().getType().equals(Material.AIR))
+            return;
         if (!(event.getAction().equals(Action.RIGHT_CLICK_AIR) || event.getAction().equals(Action.RIGHT_CLICK_BLOCK)))
             return;
         handleAction(profile, event.getItem());
@@ -44,22 +47,33 @@ public class ItemListener implements Listener {
 
     @EventHandler
     public void onInventoryClick(InventoryClickEvent event) {
-        if (!(event.getWhoClicked() instanceof Player player)) return;
+        if (!(event.getWhoClicked() instanceof Player player))
+            return;
         Profile profile = API.getProfile(player);
         if (profile.getMatch() != null
                 && profile.getMatch().getState().equals(MatchState.IN_ROUND)
                 && profile.getState() != ProfileState.IN_SPECTATOR) {
             return;
         }
-        if (profile.hasState(ProfileState.IN_CUSTOM, ProfileState.IN_KIT_EDITOR)) return;
-        if (player.getGameMode().equals(GameMode.CREATIVE)) return;
-        if (event.getClickedInventory().getType() != InventoryType.CRAFTING && event.getClickedInventory() != event.getWhoClicked().getInventory()) return;
-        event.setCancelled(true);
-        if (event.getCurrentItem() != null && event.getCursor() != null && event.getCurrentItem().getType() == Material.AIR && event.getCursor().getType() == Material.AIR)
+        if (profile.hasState(ProfileState.IN_CUSTOM, ProfileState.IN_KIT_EDITOR))
             return;
-        ItemStack item = event.getCurrentItem() == null || event.getCurrentItem().getType() == Material.AIR ? event.getCursor() : event.getCurrentItem();
-        if (item == null) return;
-        if (item.getType().equals(Material.AIR)) return;
+        if (player.getGameMode().equals(GameMode.CREATIVE))
+            return;
+        if (event.getClickedInventory() != null &&
+                event.getClickedInventory().getType() != InventoryType.CRAFTING
+                && event.getClickedInventory() != event.getWhoClicked().getInventory())
+            return;
+        event.setCancelled(true);
+        if (event.getCurrentItem() != null && event.getCursor() != null
+                && event.getCurrentItem().getType() == Material.AIR && event.getCursor().getType() == Material.AIR)
+            return;
+        ItemStack item = event.getCurrentItem() == null || event.getCurrentItem().getType() == Material.AIR
+                ? event.getCursor()
+                : event.getCurrentItem();
+        if (item == null)
+            return;
+        if (item.getType().equals(Material.AIR))
+            return;
 
         handleAction(profile, item);
     }
@@ -73,21 +87,26 @@ public class ItemListener implements Listener {
                 && profile.getState() != ProfileState.IN_SPECTATOR) {
             return;
         }
-        if (profile.hasState(ProfileState.IN_CUSTOM, ProfileState.IN_KIT_EDITOR)) return;
-        if (player.getGameMode().equals(GameMode.CREATIVE)) return;
+        if (profile.hasState(ProfileState.IN_CUSTOM, ProfileState.IN_KIT_EDITOR))
+            return;
+        if (player.getGameMode().equals(GameMode.CREATIVE))
+            return;
         event.setCancelled(true);
-        if (event.getOffHandItem() == null) return;
-        if (event.getOffHandItem().getType().equals(Material.AIR)) return;
+        if (event.getOffHandItem() == null)
+            return;
+        if (event.getOffHandItem().getType().equals(Material.AIR))
+            return;
 
         handleAction(profile, event.getOffHandItem());
     }
 
     private void handleAction(Profile profile, ItemStack item) {
         Item clickedItem = Item.getByItemStack(profile.getState(), item, profile.getPlayerUUID());
-        if (clickedItem == null) return;
+        if (clickedItem == null)
+            return;
 
-        if (!profile.hasCooldownEnded("hotbar")) return;
-
+        if (!profile.hasCooldownEnded("hotbar"))
+            return;
 
         if (clickedItem instanceof CustomItem customItem) {
             String command = customItem.getCommand();
